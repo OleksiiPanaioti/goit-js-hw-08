2f5b625f315680844b2d545626f388526ae34ba4
OleksiiPanaioti <cavedzgn@gmail.com>
2023-02-25 16:57:23 +0200

    added localstorage for currenttime, and modified for remembering the time after page reload

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,41 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function() {
+    console.log('played the video!');
+
+
+});
+
+player.on('timeupdate', function (timeupdate) {
+    const dataTimeValue = localStorage.setItem('videoplayer-current-time', JSON.stringify(timeupdate))
+  
+})
+// беремо час після паузи
+    const timePaused = localStorage.getItem('videoplayer-current-time');
+const parsedTime = JSON.parse(timePaused);
+const secondsEl = parsedTime.seconds;
+    
+console.log(parsedTime.seconds);
+
+//починаємо відео з місця паузи після перезавантаження
+
+player.setCurrentTime(secondsEl).then(function(seconds) {
+// seconds = the actual time that the player seeked to
+
+
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
